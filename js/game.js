f852410a0eff666074192925a38cff23e262847a
matthiasmulzet <mulzetmatthias@gmail.com>
2022-10-02 22:12:46 +0200

    first commit

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let ctx;
+let character = new MovableObject();
+
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    console.log('my character is:', character);
+
+}
