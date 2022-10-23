cb7fa19fda24ca14dffbd9ac7f7858e964efafa2
matthiasmulzet <mulzetmatthias@gmail.com>
2022-10-23 13:11:10 +0200

    implement controls key

--- js/startscreen.js
+++ js/startscreen.js
@@ -1,3 +1,9 @@
-// function showControlls {
+function showControls() {
+    document.getElementById('controls').classList.remove('d-none');
+    document.getElementById('startscreen-img').style = 'filter: grayscale(50%);';
+}
 
-// }
+function closeControls() {
+    document.getElementById('controls').classList.add('d-none');
+    document.getElementById('startscreen-img').style = 'filter: grayscale(0%);';
+}
